9bf523d914071464459118d1da3664a83b206650
vl_st_ <roman.nikolyuk@gmail.com>
2022-02-28 23:49:27 +0200

    Initial commit

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "21.01.2004"; // Дата рождения
+var fullname = "Почтарук Владислав Віталійович" // ФИО
+var name = "Почта" // Имя
+
+// Паспорт
+var passport_id = "004216204" // Номер паспорта
+
+// КПП
+var kpp_id = "3857109110" // ИНН
+
+// Права
+var rights_valid_until = "16.11.2024" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "04.05.2022" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
